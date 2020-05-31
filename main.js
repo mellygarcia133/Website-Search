938dcbebb45574dc78886da82036138d53f2a6ef
Melissa Garcia <mellygarcia133@users.noreply.github.com>
2020-05-31 12:41:23 -0700

    added main.js page and updated html

---
+++ main.js
@@ -0,0 +1,6 @@
+var API_URL;
+var getInputValue = "";
+var giphyAPI_url = "https://api.giphy.com/v1/gifs/search?q=";
+var giphyAPI_key = "0OBXJDQAUmPWs9DiNSHujXbpy0Vo9h5R=";
+var getSearchNum = "";
+var searchItem = "";
